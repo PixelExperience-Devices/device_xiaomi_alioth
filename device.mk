dd8c58fae295c615144a50362fc4334ed2cb07ef
Zinadin Zidan <zidan.roking@gmail.com>
2021-08-21 23:57:54 +0600

    alioth: PixelExperience Bringup
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>

--- device.mk
+++ device.mk
@@ -41,7 +41,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
