7a7f7ded68253fd8ef98cfdcfb334e3beee940c3
chandra <chandra.chaganti@hotmail.com>
2022-01-21 11:11:55 +0530

    alioth: PixelExperience Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_alioth.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_alioth-user \
-    lineage_alioth-userdebug \
-    lineage_alioth-eng
+    $(LOCAL_DIR)/aosp_alioth.mk
