cfd0edd123e83c61c4e942588d3906b98872385d
Ramii Ahmed <ramy@ahmedramy.com>
2022-01-23 22:45:44 +0200

    alioth: releasetools: Decommonize releasetools extensions
    Change-Id: I3e98e30154f6b55493f4a6fa9dc0f72334b26ff7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,6 +30,9 @@ TARGET_OTA_ASSERT_DEVICE := alioth|aliothin
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
+
 # Inherit from the proprietary version
 include vendor/xiaomi/alioth/BoardConfigVendor.mk
 # Inherit firmware
