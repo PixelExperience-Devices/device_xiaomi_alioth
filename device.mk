8844cc249102b7f43e4c720e39abca0855d45493
Alexander Koskovich <akoskovich@pm.me>
2022-03-06 02:11:47 -0700

    alioth: Update to Boot Control HAL v1.2

--- device.mk
+++ device.mk
@@ -16,9 +16,9 @@ TARGET_IS_VAB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
