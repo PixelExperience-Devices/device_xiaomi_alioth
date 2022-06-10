d0f768b2fc6ec3664e2cd82e9884380083dbfa9d
Kuba Wojciechowski <nullbytepl@gmail.com>
2022-06-10 20:36:28 +0530

    alioth: Disable kpti
    * As per qualcomm - "SM8150/SM8250/SM8350/SM7250/SM7150/SM6150 - KPTI Not
    required".
    * It can also help increase performance by a lot in some scenarios.
    
    Change-Id: Ie8a1bc184e7e82e98ea9bda7da26bd96d02fdb00

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,6 +94,7 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm
 BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=qcom
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += kpti=off
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
