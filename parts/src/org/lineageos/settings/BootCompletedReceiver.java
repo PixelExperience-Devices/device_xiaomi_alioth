031ed70501a0ea0d9ec44c05a6f42b287d081159
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-03-10 21:07:29 +0200

    alioth: parts: Avoid unsafe boot completed receiver
    Ref: https://stackoverflow.com/questions/52628780/how-to-fix-unsafeprotectedbroadcastreceiver
    Reported-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Change-Id: I3449f149ec4ca7a9873f9f95b1f1f188b6fe3ff2

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -33,6 +33,9 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+        if (!intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            return;
+        }
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
